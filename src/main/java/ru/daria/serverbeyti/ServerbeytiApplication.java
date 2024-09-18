828aeb08013a9ff2e4cb8571e11539bafe84cd13
Darya Belousova <136568577+Dariagb@users.noreply.github.com>
2024-09-18 10:49:51 +0300

    заработало

--- src/main/java/ru/daria/serverbeyti/ServerbeytiApplication.java
+++ src/main/java/ru/daria/serverbeyti/ServerbeytiApplication.java
@@ -2,8 +2,10 @@ package ru.daria.serverbeyti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ServerbeytiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerbeytiApplication.class, args);
--- src/main/java/ru/daria/serverbeyti/feinKlient/OxideService.java
+++ src/main/java/ru/daria/serverbeyti/feinKlient/OxideService.java
@@ -1,15 +1,14 @@
 package ru.daria.serverbeyti.feinKlient;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class OxideService {
+
     private final OxideClient oxideClient;
-     @Autowired
+
     public OxideService(OxideClient oxideClient) {
         this.oxideClient = oxideClient;
     }
@@ -20,4 +19,6 @@ public class OxideService {
         return product;
     }
 }
+
+
 
